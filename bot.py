85c1ac029cdf577cc2ad815c44e1252815a00f8b
dar <darkeynight@gmail.com>
2026-06-25 13:09:00 +0500

    Change Chrome version to newer

--- bot.py
+++ bot.py
@@ -101,7 +101,7 @@ async def parse_olx_first_price(url: str) -> str:
 
     async with await browser.new_context(
         viewport={"width": 1920, "height": 1080},
-        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
     ) as context:
 
         page = await context.new_page()
